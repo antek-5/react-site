7efe1aca7d829286e27dd64ec232472c3fc333c7
Antoni Jankowski <antek.jankowski.jv@gmail.com>
2024-06-22 23:39:07 +0200

    Redux Added

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
-/* STYLES */
-
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import store from './redux/store'
+import App from './App';
 import './styles/normalize.scss';
 import './styles/global.scss';
 import 'font-awesome/css/font-awesome.min.css';
 
-/* JAVA SCRIPT */
-
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <App />
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
 );
